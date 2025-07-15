e7e4663119c66e59f7451a00ad3385e6b9d79e46
Timka711 <62479085+Timka711@users.noreply.github.com>
2025-07-15 08:43:08 +0300

    Updated QuestionTree.tsx

--- src/components/QuestionTree.tsx
+++ src/components/QuestionTree.tsx
@@ -105,19 +105,13 @@ export function QuestionTree() {
   }
 
   const handleQuestionClick = (question: Question) => {
-    if (hasSubtopics(question.id)) {
-      // If question has subtopics, show board
-      const newBreadcrumbs = [...breadcrumbs, question];
-      setBreadcrumbs(newBreadcrumbs);
-      setSelectedQuestion(question.id);
-      setBoardStack([question.id]);
-    } else {
-      // If no subtopics, just navigate normally
-      const newBreadcrumbs = [...breadcrumbs, question];
-      setBreadcrumbs(newBreadcrumbs);
-      setSelectedQuestion(question.id);
-    }
-  };
+  const newBreadcrumbs = [...breadcrumbs, question];
+  setBreadcrumbs(newBreadcrumbs);
+  setSelectedQuestion(question.id);
+  
+  // Всегда добавляем вопрос в стек досок
+  setBoardStack(prev => [...prev, question.id]); 
+};
 
   const handleNavigateToSubboard = (questionId: number) => {
     const question = questions.find(q => q.id === questionId);
