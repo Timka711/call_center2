bf9abedd7be1a81bfee9ee9232dd824bf0195705
Timka711 <62479085+Timka711@users.noreply.github.com>
2025-07-25 15:39:28 +0300

    Updated WaveBackground.tsx

--- src/components/WaveBackground.tsx
+++ src/components/WaveBackground.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 
 export function WaveBackground() {
- <script src="three.r134.min.js"></script>
-<script src="vanta.clouds2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.clouds2.min.js"></script>
 <script>
-  VANTA.CLOUDS2({
-    el: "#your-element-selector",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    texturePath: "./gallery/noise.png"
-  })
+var setVanta = ()=>{
+if (window.VANTA) window.VANTA.CLOUDS2({
+  el: ".s-page-1 .s-section-1 .s-section",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  texturePath: "./gallery/noise.png"
+})
+}
+_strk.push(function() {
+  setVanta()
+  window.edit_page.Event.subscribe( "Page.beforeNewOneFadeIn", setVanta )
+})
 </script>
 }
